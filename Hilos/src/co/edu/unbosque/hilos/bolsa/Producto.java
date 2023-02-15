cb9b1dc8498d7b63d9f8cdc4836929261b69bdd0
rcamargol <rcamargol@unbosque.edu.co>
2023-02-14 21:44:55 -0500

    inclusión ejercicio bolsa

---
+++ Hilos/src/co/edu/unbosque/hilos/bolsa/Producto.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.hilos.bolsa;
+
+public class Producto {
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
