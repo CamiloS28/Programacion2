412f987a9b9bedca8c10c7ee24a85a51943f3ff0
rcamargol <rcamargol@unbosque.edu.co>
2023-05-14 22:08:04 -0500

    varios

--- ProyectoHibernate1/src/co/edu/unbosque/model/ConsultaClientes.java
+++ ProyectoHibernate1/src/co/edu/unbosque/model/ConsultaClientes.java
@@ -2,11 +2,15 @@ package co.edu.unbosque.model;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class ConsultaClientes {
+	
+	private List<Clientes> listaClientes;
 	
 	public static void main(String[] args) {
 		SessionFactory miFactory = new Configuration()
@@ -24,7 +28,9 @@ public class ConsultaClientes {
 					miSession.createQuery("from Clientes where direccion='Calle2' or nombre='Alberto'")
 					.getResultList();
 			listarClientes(listaClientes);
+			System.out.println("Resultado:"+generarId());
 			miSession.getTransaction().commit();
+			
 			miSession.close();
 		}
 		catch(Exception e) {
@@ -39,5 +45,50 @@ public class ConsultaClientes {
 		for(Clientes cliente:listaClientes) {
 			System.out.println(cliente);
 		}
+	}
+	
+	public static int generarId() {
+		int id;
+		SessionFactory miFactory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Clientes.class)
+				.buildSessionFactory();
+		Session miSession = miFactory.openSession();
+
+		try {
+			miSession.beginTransaction();
+			Query query = miSession.createQuery("select max(id)+1 as id from Clientes");
+			List numeros = query.getResultList();
+			id = (int)numeros.get(0);
+			miSession.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			id = -1;
+		} finally {
+			miFactory.close();
+		}		
+		return id;		
+	}
+	
+	public Object consultar() {
+		// TODO Auto-generated method stub
+		SessionFactory miFactory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Clientes.class)
+				.buildSessionFactory();
+		Session miSession = miFactory.openSession();
+		try {
+			miSession.beginTransaction();
+			listaClientes = miSession.createQuery("from Clientes").getResultList();
+			listarClientes(listaClientes);
+			miSession.getTransaction().commit();
+			miSession.close();
+		} catch (Exception e) {
+			listaClientes=null;
+			//resultado = e.toString();
+		} finally {
+			miFactory.close();
+		}		
+		return listaClientes;
 	}
 }
